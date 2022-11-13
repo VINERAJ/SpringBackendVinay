0a9b0eca7478bfaf0dd9492f9283f63aa5af35ce
John Mortensen <jmortensen@powayusd.com>
2022-11-13 11:01:31 -0800

    add Person database and methods

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -0,0 +1,64 @@
+package com.nighthawk.spring_portfolio.mvc.person;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.*;
+
+/*
+Person is a POJO, Plain Old Java Object.
+First set of annotations add functionality to POJO
+--- @Setter @Getter @ToString @NoArgsConstructor @RequiredArgsConstructor
+The last annotation connect to database
+--- @Entity
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Person {
+    // automatic unique identifier for Person record
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    // email, password, roles are key to login and authentication
+    @NotEmpty
+    @Size(min=5)
+    @Column(unique=true)
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    // @NonNull: Places this in @RequiredArgsConstructor
+    @NonNull
+    @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dob;
+
+    // Initializer used when setting database from an API
+    public Person(String email, String password, String name, Date dob) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.dob = dob;
+    }
+
+    // A custom getter to return age from dob calculation
+    public int getAge() {
+        if (this.dob != null) {
+            LocalDate birthDay = this.dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            return Period.between(birthDay, LocalDate.now()).getYears(); }
+        return -1;
+    }
+
+}
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -0,0 +1,95 @@
+package com.nighthawk.spring_portfolio.mvc.person;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+@RestController
+@RequestMapping("/api/person")
+public class PersonApiController {
+    /*
+    #### RESTful API ####
+    Resource: https://spring.io/guides/gs/rest-service/
+    */
+
+    // Autowired enables Control to connect POJO Object through JPA
+    @Autowired
+    private PersonJpaRepository repository;
+
+    /*
+    GET List of People
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<Person>> getPeople() {
+        return new ResponseEntity<>( repository.findAllByOrderByNameAsc(), HttpStatus.OK);
+    }
+
+    /*
+    GET individual Person using ID
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable long id) {
+        Optional<Person> optional = repository.findById(id);
+        if (optional.isPresent()) {  // Good ID
+            Person person = optional.get();  // value from findByID
+            return new ResponseEntity<>(person, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
+        }
+        // Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);       
+    }
+
+    /*
+    DELETE individual Person using ID
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Person> deletePerson(@PathVariable long id) {
+        Optional<Person> optional = repository.findById(id);
+        if (optional.isPresent()) {  // Good ID
+            Person person = optional.get();  // value from findByID
+            repository.deleteById(id);  // value from findByID
+            return new ResponseEntity<>(person, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
+        }
+        // Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
+    }
+
+    /*
+    POST Aa record by Requesting Parameters from URI
+     */
+    @PostMapping( "/post")
+    public ResponseEntity<Object> postPerson(@RequestParam("email") String email,
+                                             @RequestParam("password") String password,
+                                             @RequestParam("name") String name,
+                                             @RequestParam("dob") String dobString) {
+        Date dob;
+        try {
+            dob = new SimpleDateFormat("MM-dd-yyyy").parse(dobString);
+        } catch (Exception e) {
+            return new ResponseEntity<>(dobString +" error; try MM-dd-yyyy", HttpStatus.BAD_REQUEST);
+        }
+        // A person object WITHOUT ID will create a new record with default roles as student
+        Person person = new Person(email, password, name, dob);
+        repository.save(person);
+        return new ResponseEntity<>(email +" is created successfully", HttpStatus.CREATED);
+    }
+
+    /*
+    The personSearch API looks across database for partial match to term (k,v) passed by RequestEntity body
+     */
+    @PostMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> personSearch(@RequestBody final Map<String,String> map) {
+        // extract term from RequestEntity
+        String term = (String) map.get("term");
+
+        // JPA query to filter on term
+        List<Person> list = repository.findByNameContainingIgnoreCaseOrEmailContainingIgnoreCase(term, term);
+
+        // return resulting list and status, error checking should be added
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+}
