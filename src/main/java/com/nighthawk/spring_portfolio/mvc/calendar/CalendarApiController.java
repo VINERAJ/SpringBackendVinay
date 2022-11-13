18c1dda3314f0dfd8f45050e20c5393394f77fed
John Mortensen <jmortensen@powayusd.com>
2022-11-12 18:05:02 -0800

    formatting

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarApiController.java
@@ -1,27 +1,38 @@
 package com.nighthawk.spring_portfolio.mvc.calendar;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 // Pseudo Code / Implementation Prototypes 
 
 @RestController
 @RequestMapping("/api/calendar")
 public class CalendarApiController {
+
     /** GET isLeapYear endpoint
+     * ObjectMapper throws exceptions which should return 500 error
+     * @throws JsonProcessingException
+     * @throws JsonMappingException
      */
     @GetMapping("/isLeapYear/{year}")
-    public ResponseEntity<Year> getIsLeapYear(@PathVariable int year) {
-      // serializing leapYear to JSON not verified
+    public ResponseEntity<JsonNode> getIsLeapYear(@PathVariable int year) throws JsonMappingException, JsonProcessingException {
+      // Backend Year Object, evaluates Leap Year
       Year year_obj = new Year();
-
       year_obj.setYear(year);
 
-      return new ResponseEntity<>(year_obj, HttpStatus.OK);  // response not tested
+      // Turn Year Object toString into JSON
+      ObjectMapper mapper = new ObjectMapper(); // should move this under Try/Catch, but risk is low
+      JsonNode json = mapper.readTree(year_obj.toString());
+
+      return ResponseEntity.ok(json);  // JSON response
     }
 
     // add other methods
--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/Year.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/Year.java
@@ -2,37 +2,37 @@ package com.nighthawk.spring_portfolio.mvc.calendar;
 
 // simple POJO for serializing JSON, this could be moved into an entity
 class Year {
-    private int year;
-    private boolean isLeapYear;
+   private int year;
+   private boolean isLeapYear;
 
- 
-    public Year() {} // zero argument constructor
- 
-    /* year getter/setters */
-    public int getYear() {
-       return year;
-     }
-    public void setYear(int year) {
-        this.year = year;
-        this.setIsLeapYear(year);
-     }
- 
-    /* isLeapYear getter/setters */
-    public boolean getIsLeapYear(int year) {
-       return APCalendar.isLeapYear(year);
-     }
-    private void setIsLeapYear(int year) {  // this is private to avoid tampering
-        this.isLeapYear = APCalendar.isLeapYear(year);
-     }
- 
-    /* toString print */
-    public String toString(){
-       return "Year [ year: "+this.year+", isLeapYear: "+ this.isLeapYear+ " ]";
-     }	
+   // zero argument constructor
+   public Year() {} 
 
-     public static void main(String[] args) {
-        Year year = new Year();
-        year.setYear(2022);
-        System.out.println(year);
-     }
+   /* year getter/setters */
+   public int getYear() {
+      return year;
+   }
+   public void setYear(int year) {
+      this.year = year;
+      this.setIsLeapYear(year);
+   }
+
+   /* isLeapYear getter/setters */
+   public boolean getIsLeapYear(int year) {
+      return APCalendar.isLeapYear(year);
+   }
+   private void setIsLeapYear(int year) {  // this is private to avoid tampering
+      this.isLeapYear = APCalendar.isLeapYear(year);
+   }
+
+   /* toString formatted to map to JSON */
+   public String toString(){
+      return ( "{ \"year\": "  +this.year+  ", " + "\"isLeapYear\": "  +this.isLeapYear+ " }" );
+   }	
+
+   public static void main(String[] args) {
+      Year year = new Year();
+      year.setYear(2022);
+      System.out.println(year);
+   }
 }
