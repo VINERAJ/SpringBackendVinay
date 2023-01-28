15bd56c6a4e5f33aa6f3d00bbf7ec74ef7b11bce
John Mortensen <jmortensen@powayusd.com>
2023-01-28 07:19:42 -0800

    add Persons initialization

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -1,5 +1,6 @@
 package com.nighthawk.spring_portfolio.mvc.person;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
@@ -92,6 +93,66 @@ public class Person {
             LocalDate birthDay = this.dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
             return Period.between(birthDay, LocalDate.now()).getYears(); }
         return -1;
+    }
+
+    // Initialize static test data 
+    public static Person[] initPersons() {
+
+        // basics of class construction
+        Person p1 = new Person();
+        p1.setName("Thomas Edison");
+        p1.setEmail("toby@gmail.com");
+        p1.setPassword("123Toby!");
+        try {  // All data that converts formats could fail
+            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("01-01-1840");
+            p1.setDob(d);
+        } catch (Exception e) {
+            // no actions as dob default is good enough
+        }
+
+        Person p2 = new Person();
+        p2.setName("zAlexander Graham Bell");
+        p2.setEmail("lexb@gmail.com");
+        p2.setPassword("123LexB!");
+        try {
+            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("01-01-1845");
+            p2.setDob(d);
+        } catch (Exception e) {
+        }
+
+        Person p3 = new Person();
+        p3.setName("Nikola Tesla");
+        p3.setEmail("niko@gmail.com");
+        p3.setPassword("Niko!");
+        try {
+            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("01-01-1850");
+            p3.setDob(d);
+        } catch (Exception e) {
+        }
+
+        Person p4 = new Person();
+        p4.setName("Madam Currie");
+        p4.setEmail("madam@gmail.com");
+        p4.setPassword("123Madam!");
+        try {
+            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("01-01-1860");
+            p4.setDob(d);
+        } catch (Exception e) {
+        }
+
+        Person p5 = new Person();
+        p5.setName("John Mortensen");
+        p5.setEmail("jm1021@gmail.com");
+        p5.setPassword("123Qwerty!");
+        try {
+            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("10-21-1959");
+            p5.setDob(d);
+        } catch (Exception e) {
+        }
+
+        // Array definition and data initialization
+        Person persons[] = {p1, p2, p3, p4, p5};
+        return(persons);
     }
 
 }
