c77a9b856320a2daed5f2b067d164d845b830756
John Mortensen <jmortensen@powayusd.com>
2023-01-28 17:21:44 -0800

    formatting

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonJpaRepository.java
@@ -18,17 +18,19 @@ public interface PersonJpaRepository extends JpaRepository<Person, Long> {
 
     // JPA query, findBy does JPA magic with "Name", "Containing", "Or", "Email", "IgnoreCase"
     List<Person> findByNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String name, String email);
+
     /* Custom JPA query articles, there are articles that show custom SQL as well
        https://springframework.guru/spring-data-jpa-query/
        https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
-     */
-     Person findByEmailAndPassword(String email, String password);
+    */
+    Person findByEmailAndPassword(String email, String password);
+
     // Custom JPA query
     @Query(
             value = "SELECT * FROM Person p WHERE p.name LIKE ?1 or p.email LIKE ?1",
             nativeQuery = true)
     List<Person> findByLikeTermNative(String term);
     /*
-        https://www.baeldung.com/spring-data-jpa-query
-     */
+      https://www.baeldung.com/spring-data-jpa-query
+    */
 }
