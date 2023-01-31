7c27887f85b5372918ac04df5269ef97a5fc10a7
John Mortensen <jmortensen@powayusd.com>
2023-01-31 05:20:52 -0800

    adding in CRUD

--- src/main/java/com/nighthawk/spring_portfolio/Main.java
+++ src/main/java/com/nighthawk/spring_portfolio/Main.java
@@ -3,7 +3,7 @@ package com.nighthawk.spring_portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication annotation is key to building web applications with Java https://spring.io/projects/spring-boot
+// @SpringBootApplication annotation is the key to building web applications with Java https://spring.io/projects/spring-boot
 @SpringBootApplication
 public class Main {
 
