de71291ad388f3d8703d03a860b29cb90c6a5080
John Mortensen <jmortensen@powayusd.com>
2023-02-01 14:53:51 -0800

    comment

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonDetailsService.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonDetailsService.java
@@ -32,13 +32,14 @@ public class PersonDetailsService implements UserDetailsService {  // "implement
     @Override
     public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Person person = personJpaRepository.findByEmail(email); // setting variable user equal to the method finding the username in the database
-        if(person==null){
+        if(person==null) {
 			throw new UsernameNotFoundException("User not found with username: " + email);
         }
         Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
         person.getRoles().forEach(role -> { //loop through roles
             authorities.add(new SimpleGrantedAuthority(role.getName())); //create a SimpleGrantedAuthority by passed in role, adding it all to the authorities list, list of roles gets past in for spring security
         });
+        // train spring security to User and Authorities
         return new org.springframework.security.core.userdetails.User(person.getEmail(), person.getPassword(), authorities);
     }
 
@@ -64,6 +65,7 @@ public class PersonDetailsService implements UserDetailsService {  // "implement
         return personJpaRepository.findByLikeTermNative(like_term);
     }
 
+    // encode password prior to sava
     public void save(Person person) {
         person.setPassword(passwordEncoder.encode(person.getPassword()));
         personJpaRepository.save(person);
