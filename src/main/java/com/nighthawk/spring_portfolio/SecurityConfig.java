2272abb5ee70d62361c8b1b2f39cfaa267edfa8f
John Mortensen <jmortensen@powayusd.com>
2023-02-04 09:10:44 -0800

    revert

--- src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -66,13 +66,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		httpSecurity
 			// no CSRF
 			.csrf().disable()
-			/* 
 			// list the requests/endpoints need to be authenticated
 			.authorizeRequests()
 				.antMatchers("/mvc/person/update/**", "/mvc/person/delete/**").authenticated()
 				.antMatchers("/api/person/update/**", "/api/person/delete/**").authenticated()
 				.and()
-			// support cors
+			/* 
+			// support cors on localhost
 			.cors().and()
 			.headers()
 				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
