d01aefc34e4fa60e433234935bc5e91d8f0213ac
John Mortensen <jmortensen@powayusd.com>
2023-02-06 23:12:02 -0800

    back

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -24,7 +24,7 @@ public class MvcConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://nighthawkcoders.github.io");
+        registry.addMapping("/**").allowedOrigins("https://nighthawkcoders.github.io", "http://localhost:4000");
     }
     
 }
