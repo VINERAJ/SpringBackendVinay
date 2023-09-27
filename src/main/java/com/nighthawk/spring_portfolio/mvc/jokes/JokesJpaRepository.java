4874ce82a8c40a63020a39b01132206d43fcc89f
John Mortensen <jmortensen@powayusd.com>
2023-09-27 09:22:54 -0700

    add comments

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/JokesJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/JokesJpaRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // JPA is an object-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
 public interface JokesJpaRepository extends JpaRepository<Jokes, Long> {
-    // JPA has many built in methods, these few have been prototyped for this application
+    /* JPA has many built in methods: https://www.tutorialspoint.com/spring_boot_jpa/spring_boot_jpa_repository_methods.htm
+    The below custom methods are prototyped for this application
+    */
     void save(String Joke);
     List<Jokes> findAllByOrderByJokeAsc();
     List<Jokes> findByJokeIgnoreCase(String joke);
