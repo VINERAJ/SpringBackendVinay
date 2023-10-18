3e5281d20d46cc16b482aaad4f36fda8e1013975
aidanywu <aidanywu@gmail.com>
2023-10-18 01:25:40 -0700

    spring security 6 migration

--- src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -11,12 +11,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
@@ -28,8 +29,8 @@ import org.springframework.security.web.header.writers.StaticHeadersWriter;
 */
 @Configuration
 @EnableWebSecurity  // Beans to enable basic Web security
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
 
     @Autowired
 	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
@@ -53,52 +54,51 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(personDetailsService).passwordEncoder(passwordEncoder());
 	}
 
-	@Override
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+	// @Bean
+	// public AuthenticationManager authenticationManagerBean() throws Exception {
+	// 	return super.authenticationManagerBean();
+	// }
 
 	
     // Provide security configuration
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity
-			// no CSRF
-			.csrf().disable()
-			// list the requests/endpoints need to be authenticated
-			.authorizeRequests()
-				.antMatchers("/mvc/person/update/**", "/mvc/person/delete/**").authenticated()
-				.antMatchers("/api/person/**").authenticated()
-				.and()
-			// support cors
-			.cors().and()
-			.headers()
-				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
-				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
-				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type", "Authorization", "x-csrf-token"))
-				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
-				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET", "OPTIONS", "HEAD"))
-				//.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "https://nighthawkcoders.github.io", "http://localhost:4000"))
-				.and()
-			.formLogin()
-                .loginPage("/login")
-                .and()
-            .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-				.and()
-			// make sure we use stateless session; 
-			// session won't be used to store user's state.
-			.exceptionHandling()
-				.authenticationEntryPoint(jwtAuthenticationEntryPoint)
-				.and()
-				.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)           
-		;
-
-		// Add a filter to validate the tokens with every request
-		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-
+		@Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			http
+				.csrf(csrf -> csrf
+					.disable()
+				)
+				// list the requests/endpoints need to be authenticated
+				.authorizeHttpRequests(auth -> auth
+					.requestMatchers("/mvc/person/update/**", "/mvc/person/delete/**").authenticated()
+					.requestMatchers("/api/person/**").authenticated()
+				)
+				// support cors
+				.cors(Customizer.withDefaults())
+				.headers(headers -> headers
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type", "Authorization", "x-csrf-token"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET", "OPTIONS", "HEAD"))
+					//.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "https://nighthawkcoders.github.io", "http://localhost:4000"))
+				)
+				.formLogin(form -> form 
+					.loginPage("/login")
+				)
+				.logout(logout -> logout
+					.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+					.logoutSuccessUrl("/")
+				)
+				// make sure we use stateless session; 
+				// session won't be used to store user's state.
+				.exceptionHandling(exceptions -> exceptions
+					.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+				)
+				.sessionManagement(session -> session
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				)
+				// Add a filter to validate the tokens with every request
+				.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+			return http.build();
 	}
 }
